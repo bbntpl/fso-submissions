2f9a41749cd05b16b5f976c78d2e5e4609407624
bvrbryn445 <law911012eqw@gmail.com>
2022-03-26 20:12:42 -0400

    add exercise 2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -20,11 +20,12 @@ const App = () => {
 	//get the response from the data header using axios and 
 	//initialize both persons and filtered persons as its initial data
 	useEffect(() => {
-		axiosServices.getAll().then(returnedPersons => {
-			setPersons(returnedPersons);
-			setFilteredPersons(returnedPersons);
-		})
-		.catch(error => console.log(`error: ${error}`));
+		axiosServices.getAll()
+			.then(returnedPersons => {
+				setPersons(returnedPersons);
+				setFilteredPersons(returnedPersons);
+			})
+			.catch(error => console.log(`error: ${error}`));
 	}, []);
 
 	function addName(name) {
@@ -34,8 +35,7 @@ const App = () => {
 
 	function addNumber(number) {
 		if (!number) return;
-		//converting a phone/tel number to a text
-		setNewPerson({ ...newPerson, number: number + '' });
+		setNewPerson({ ...newPerson, number: number });
 	}
 
 	function addPerson(newPerson, persons) {
@@ -45,21 +45,20 @@ const App = () => {
 		axiosServices.create(newPerson)
 			.then(returnedPersons => {
 				setPersons(persons.concat(returnedPersons));
+				setFilteredPersons(persons.concat(returnedPersons));
 			})
 		setNewPerson({ ...newPerson, id: newPerson.length + 1 });
-		setFilteredPersons(persons.concat(newPerson));
-		clearInputs();
 	}
 
 	const deletePerson = (id, name) => {
 		if (window.confirm(`Delete ${name}`)) {
 			axiosServices
-			.delete(id)
-			.then(returnedPersons => {
-				const deleteById = person => person.id !== id;
-				setPersons(persons.filter(deleteById));
-				setFilteredPersons(persons.filter(deleteById));
-			})
+				.delete(id)
+				.then(returnedPersons => {
+					const deleteById = person => person.id !== id;
+					setPersons(persons.filter(deleteById));
+					setFilteredPersons(persons.filter(deleteById));
+				})
 		} else {
 			console.log(`Canceled deletion of ${name}'s number`);
 		}
@@ -69,20 +68,41 @@ const App = () => {
 		setNewPerson({ ...newPerson, name: '', number: '' });
 	}
 
-	function alertIfNameAlreadyExists() {
-		alert(`${newPerson.name} is already added to phonebook`);
+	// propmpt the user about existing person
+	// and ask to replace the number or not
+	function confirmReplaceNumber(name) {
+		return window.confirm(`${name} is already added to phonebook, replace the old number with a new one?`);
 	}
+	const filterByPropValue = (propKey) => persons.filter(person => person[propKey] === newPerson[propKey]);
+	const isPersonNameExists = filterByPropValue('name').length;
 
-	const isNameExists = () => persons.filter(person => person.name === newPerson.name).length;
 
+	const replaceOldNumber = (person, newPerson) => {
+		const { id } = person;
+		const { number, name } = newPerson;
+		if (confirmReplaceNumber(name)) {
+			const modifiedInfo = { ...person, number: number };
+			axiosServices
+				.update(id, modifiedInfo)
+				.then(returnedPerson => {
+					const modifyCopyOfPersons = () => persons.map(person => {
+						return person.id === id ? returnedPerson : person;
+					})
+					setPersons(modifyCopyOfPersons);
+					setFilteredPersons(modifyCopyOfPersons);
+				})
+		}
+	}
 	function submitForm(e) {
 		e.preventDefault();
 		if (!newPerson.name.length) return;
-		if (isNameExists()) {
-			alertIfNameAlreadyExists();
+		if (isPersonNameExists) {
+			const existingPerson = filterByPropValue('name')[0];
+			replaceOldNumber(existingPerson, newPerson);
 		} else {
 			addPerson(newPerson, persons);
 		}
+		clearInputs();
 	}
 
 	return (
--- part2/phonebook/src/components/Contacts.jsx
+++ part2/phonebook/src/components/Contacts.jsx
@@ -1,17 +1,26 @@
 import PersonDetails from './PersonDetails';
+const displayEmptyPhonebook = () => {
+	return (
+		<p>You don't have any person on your contact list.</p>
+	)
+}
+const displayPhonebook = (persons, deletePerson) => {
+	return persons.map(person => {
+		const { id, name } = person;
+		return (
+			<div key={id}>
+				<PersonDetails person={person} />
+				<button onClick={() => deletePerson(id, name)}>delete</button>
+			</div>)
+	})
+}
 
 const Contacts = ({ persons, deletePerson }) => {
 	return (
 		<div>
 			{
-				persons.map(person => {
-					const { id, name } = person;
-					return (
-						<div key={id}>
-							<PersonDetails person={person} />
-							<button onClick={() => deletePerson(id, name)}>delete</button>
-						</div>)
-				})
+			!persons.length ? displayEmptyPhonebook()
+			: displayPhonebook(persons, deletePerson)
 			}
 		</div>
 	)
