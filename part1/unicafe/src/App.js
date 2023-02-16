5c0c3fbb217568bbba7913d13a4374d1cd03adc7
bvrbryn445 <beaverbryanantipolo@gmail.com>
2023-02-15 23:21:22 -0500

    Update: unicafe (part 1) reattempt

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,89 +1,84 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from 'react'
 
-const totalFeedbacks = ({ good, neutral, bad }) => good + neutral + bad;
-const averageFeedback = (feedbackOpts) => {
-	const { good, bad } = feedbackOpts;
-	const averageScore = good - bad;
-	return (averageScore / totalFeedbacks(feedbackOpts));
-}
-const positiveFeedbackPerc = (feedbackOpts) => {
-	return (`${(feedbackOpts.good / totalFeedbacks(feedbackOpts)) * 100} %`);
-}
-
-//app components
-const Block = ({ headerName }) => <h1 className={'block-header'}>{headerName}</h1>
-
-const StatisticLine = ({ text, value }) => {
+const Button = ({ handleClick, text }) => {
 	return (
-		<tr className="stat-line">
-			<th>{text}</th>
-			<th>{value}</th>
-		</tr>
+		<button onClick={handleClick}>{text}</button>
 	)
 }
 
+// Display a line of a statistic
+const StatisticLine = ({ text, value }) => (
+	<tr>
+		<td>{text}</td>
+		<td>{value}</td>
+	</tr>
+)
 
-const FeedbackBtn = ({ incrementNum, feedbackOptName }) =>
-	<button onClick={incrementNum}>{feedbackOptName}</button>
+// Display the statistics section
+const Statistics = ({ feedbacks }) => {
+	const {
+		good,
+		neutral,
+		bad
+	} = feedbacks;
 
-const FeedbackBtns = ({ feedbackOpts, setFeedbackOpts }) => {
+	const sumOfArrEls = (feedbacks) => {
+		const add = (a, b) => a += b;
+		return feedbacks.reduce(add);
+	}
+
+	const total = sumOfArrEls([good, neutral, bad]);
+
+	const averageScore = (good - bad) / total || 0;
+
+	const positiveScore = ((good / total) * 100) || 0;
+
 	return (
 		<div>
-			<FeedbackBtn
-				incrementNum={() => setFeedbackOpts({ ...feedbackOpts, good: feedbackOpts.good + 1 })}
-				feedbackOptName={'good'}
-			/>
-			<FeedbackBtn
-				incrementNum={() => setFeedbackOpts({ ...feedbackOpts, neutral: feedbackOpts.neutral + 1 })}
-				feedbackOptName={'neutral'}
-			/>
-			<FeedbackBtn
-				incrementNum={() => setFeedbackOpts({ ...feedbackOpts, bad: feedbackOpts.bad + 1 })}
-				feedbackOptName={'bad'}
-			/>
+			<h1>statistics</h1>
+			{total ?
+				<table>
+					<StatisticLine text='good' value={good} />
+					<StatisticLine text='neutral' value={neutral} />
+					<StatisticLine text='bad' value={bad} />
+					<StatisticLine text='all' value={total} />
+					<StatisticLine text='average' value={`${averageScore} %`} />
+					<StatisticLine text='positive' value={`${positiveScore} %`} />
+				</table>
+				:
+				<p>No feedback given</p>
+			}
 		</div>
 	)
 }
 
-const Statistics = ({ feedbackOpts }) => {
-	if (!totalFeedbacks(feedbackOpts)) {
-		return (<p>No feedback given</p>);
-	}
-	return (
-		<table>
-			<tbody>
-				<StatisticLine text='good' value={feedbackOpts.good} />
-				<StatisticLine text='neutral' value={feedbackOpts.neutral} />
-				<StatisticLine text='bad' value={feedbackOpts.bad} />
-				<StatisticLine text='all' value={totalFeedbacks(feedbackOpts)} />
-				<StatisticLine text='average' value={averageFeedback(feedbackOpts)} />
-				<StatisticLine text='positive' value={positiveFeedbackPerc(feedbackOpts)} />
-			</tbody>
-		</table>
-	)
-}
 
 const App = () => {
 	// save clicks of each button to its own state
-	const [feedbackOpts, setFeedbackOpts] = useState({
+	const [feedbacks, setFeedbacks] = useState({
 		good: 0,
 		neutral: 0,
-		bad: 0
-	})
+		bad: 0,
+	});
+
+	const incrementFeedback = (type) => {
+		setFeedbacks(feedbacks => ({
+			...feedbacks,
+			[type]: feedbacks[type] + 1,
+		}));
+	}
 
 	return (
 		<div>
 			<div>
-				<Block headerName={'give feedback'} />
-				<FeedbackBtns feedbackOpts={feedbackOpts} setFeedbackOpts={setFeedbackOpts} />
+				<h1>give feedback</h1>
+				<Button handleClick={() => incrementFeedback('good')} text='good' />
+				<Button handleClick={() => incrementFeedback('neutral')} text='neutral' />
+				<Button handleClick={() => incrementFeedback('bad')} text='bad' />
 			</div>
-			<div>
-				<Block headerName={'statistics'} />
-				<Statistics feedbackOpts={feedbackOpts} />
-			</div>
+			<Statistics feedbacks={feedbacks} />
 		</div>
-	);
+	)
 }
 
 export default App;
